95c6ba23df7b23c77ba8622cd378454ce1761401
hoangdz1005 <nnh10052002@gmail.com>
2023-06-08 20:20:15 +0700

    exception

---
+++ src/main/java/com/springboot/ExceptionHandling/ExceptionHandling.java
@@ -0,0 +1,12 @@
+package com.springboot.ExceptionHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandling {
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandling.class, args);
+	}
+
+}
